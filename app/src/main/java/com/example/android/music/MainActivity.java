539b51759451b33438daee2fec8ff79387296a40
Rana Emad <rana3emad@gmail.com>
2019-11-19 22:17:00 +0200

    song activity

--- app/src/main/java/com/example/android/music/MainActivity.java
+++ app/src/main/java/com/example/android/music/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,14 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<Song> songs= new ArrayList<Song>();
+        final ArrayList<Song> songs= new ArrayList<Song>();
         songs.add(new Song("Song 1","Artist 1"));
         songs.add(new Song("Song 2","Artist 2"));
 
         SongAdaptor songAd= new SongAdaptor(this,songs);
 
         ListView listView=(ListView) findViewById(R.id.listview_song);
+
         listView.setAdapter(songAd);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, SongActivity.class);
+                intent.putExtra("name", songs.get(i).name);
+                intent.putExtra("artist", songs.get(i).artist);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/android/music/SongActivity.java
+++ app/src/main/java/com/example/android/music/SongActivity.java
@@ -3,6 +3,9 @@ package com.example.android.music;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SongActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class SongActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
+
+        TextView name = findViewById(R.id.name);
+        name.setText(getIntent().getStringExtra("name"));
+
+        TextView artist = findViewById(R.id.artist);
+        artist.setText(getIntent().getStringExtra("artist"));
+
+        final Button button = (Button) findViewById(R.id.play);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               if(button.getText().equals("PLAY")){
+                   button.setText("PAUSE");
+               }
+               else{
+                   button.setText("PLAY");
+               }
+            }
+        });
     }
 }
